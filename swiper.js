85be39ea36e0cccebd68318c9f70d7e8a814ffa3
Natasha <natkamonte1992@gmail.com>
2022-08-29 23:07:07 +0300

    .

--- swiper.js
+++ swiper.js
@@ -1,12 +1,11 @@
 const swiper = new Swiper('.swiper', {
-  // Optional parameters
-  direction: 'horizontal',
-  loop: true,
-
-  // If we need pagination
-  pagination: {
-    el: '.swiper-pagination',
+  // Accessibility
+  a11y: {
+    prevSlideMessage: 'Previous slide',
+    nextSlideMessage: 'Next slide',
   },
+
+  parallax: true,
 
   // Navigation arrows
   navigation: {
@@ -14,8 +13,75 @@ const swiper = new Swiper('.swiper', {
     prevEl: '.swiper-button-prev',
   },
 
-  // And if we need scrollbar
-  scrollbar: {
-    el: '.swiper-scrollbar',
+  // // Autoplay
+  autoplay: {
+    delay: 5000,
   },
+
+  speed: 600,
+
+  loop: true,
+
+  loopedSlides: 4,
+
+  effect: 'creative',
+  creativeEffect: {
+    prev: {
+      // will set `translateZ(-400px)` on previous slides
+      translate: [0, 0, -1500],
+    },
+    next: {
+      // will set `translateX(100%)` on next slides
+      translate: ['100%', 0, 0],
+    },
+  },
+  keyboard: {
+    enabled: true,
+    onlyInViewport: true,
+  },
+});
+
+// Id scroll
+// Найти все ссылки начинающиеся на #
+const anchors = document.querySelectorAll('a[href^="#"]');
+
+// Цикл по всем ссылкам
+for (let anchor of anchors) {
+  anchor.addEventListener('click', function (e) {
+    e.preventDefault(); // Предотвратить стандартное поведение ссылок
+    // Атрибут href у ссылки, если его нет то перейти к body (наверх не плавно)
+    const goto = anchor.hasAttribute('href')
+      ? anchor.getAttribute('href')
+      : 'body';
+    // Плавная прокрутка до элемента с id = href у ссылки
+    document.querySelector(goto).scrollIntoView({
+      behavior: 'smooth',
+      block: 'start',
+    });
+  });
+}
+
+const swiperCustomers = new Swiper('.swiper-customers', {
+  pagination: {
+    el: '.swiper-pagination',
+    clickable: true,
+  },
+
+  effect: 'cube',
+
+  cubeEffect: {
+    slideShadows: false,
+    shadow: false,
+    ShadowOffset: 0,
+    shadowScale: 0,
+  },
+
+  autoplay: {
+    delay: 6000,
+  },
+  loop: true,
+
+  loopedSlides: 4,
+
+  speed: 600,
 });
